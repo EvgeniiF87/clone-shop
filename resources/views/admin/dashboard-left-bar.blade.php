b3f4f4a2b9b010f53fc107588a672f076b2ca019
evgenii <evgenii@MacBook-Air-evgenii.local>
2021-04-16 14:42:19 +0600

    edit left bar dasboard

--- resources/views/admin/dashboard-left-bar.blade.php
+++ resources/views/admin/dashboard-left-bar.blade.php
@@ -1,66 +1,68 @@
+<div class="dashboard-left-bar_title">
+    <span>Панель управления</span>
+</div>
 
+<ul class="dashboard-left-bar_menu">
 
-    <ul class="dashboard-left-bar_menu">
+    <li class="dashboard-left-bar_menu_item">
+        <a href="/dashboard" class="btn">
+            <svg class="chart-bar" fill="currentColor">
+                <use xlink:href="{{ asset('/images/left-bar/chart-bar.svg') }}#Capa_1"/>
+            </svg>
 
-        <li class="dashboard-left-bar_menu_item">
-            <a href="/dashboard" class="btn">
-                <svg class="chart-bar" fill="currentColor">
-                    <use xlink:href="{{ asset('/images/left-bar/chart-bar.svg') }}#Capa_1"/>
-                </svg>
+            <sapn class="btn-text">Общая информация</sapn>
+        </a>
+    </li>
 
-                <sapn class="btn-text">Общая информация</sapn>
-            </a>
-        </li>
+    <li class="dashboard-left-bar_menu_item">
 
-        <li class="dashboard-left-bar_menu_item">
+        <a href="/dashboard/brand" class="btn">
+            <svg class="layer-group" fill="currentColor">
+                <use xlink:href="{{ asset('/images/left-bar/layer-group.svg') }}#Capa_1"/>
+            </svg>
 
-            <a href="/dashboard/brand" class="btn">
-                <svg class="layer-group" fill="currentColor">
-                    <use xlink:href="{{ asset('/images/left-bar/layer-group.svg') }}#Capa_1"/>
-                </svg>
+            <sapn class="btn-text">Управление брендами</sapn>
+        </a>
 
-                <sapn class="btn-text">Управление брендами</sapn>
-            </a>
+    </li>
 
-        </li>
+    <li class="dashboard-left-bar_menu_item">
 
-        <li class="dashboard-left-bar_menu_item">
+        <a href="#" class="btn">
+            <svg class="box-open" fill="currentColor">
+                <use xlink:href="{{ asset('/images/left-bar/box-open.svg') }}#Capa_1"/>
+            </svg>
 
-            <a href="#" class="btn">
-                <svg class="box-open" fill="currentColor">
-                    <use xlink:href="{{ asset('/images/left-bar/box-open.svg') }}#Capa_1"/>
-                </svg>
+            <sapn class="btn-text">Управление продукцией</sapn>
+        </a>
 
-                <sapn class="btn-text">Управление продукцией</sapn>
-            </a>
+    </li>
 
-        </li>
+    <li class="dashboard-left-bar_menu_item">
 
-        <li class="dashboard-left-bar_menu_item">
+        <a href="#" class="btn">
+            <svg class="users" fill="currentColor">
+                <use xlink:href="{{ asset('/images/left-bar/users.svg') }}#Capa_1"/>
+            </svg>
 
-            <a href="#" class="btn">
-                <svg class="users" fill="currentColor">
-                    <use xlink:href="{{ asset('/images/left-bar/users.svg') }}#Capa_1"/>
-                </svg>
+            <sapn class="btn-text">Пользователи</sapn>
+        </a>
 
-                <sapn class="btn-text">Пользователи</sapn>
-            </a>
+    </li>
 
-        </li>
+    <li class="dashboard-left-bar_menu_item">
 
-        <li class="dashboard-left-bar_menu_item">
+        <a href="#" class="btn">
+            <svg class="paste" fill="currentColor">
+                <use xlink:href="{{ asset('/images/left-bar/paste.svg') }}#Capa_1"/>
+            </svg>
 
-            <a href="#" class="btn">
-                <svg class="paste" fill="currentColor">
-                    <use xlink:href="{{ asset('/images/left-bar/paste.svg') }}#Capa_1"/>
-                </svg>
+            <sapn class="btn-text">Заказы</sapn>
+        </a>
 
-                <sapn class="btn-text">Заказы</sapn>
-            </a>
+    </li>
 
-        </li>
-
-    </ul>
+</ul>
 
 
 
