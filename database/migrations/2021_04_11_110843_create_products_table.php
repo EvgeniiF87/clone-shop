c415e3f89f8113e0cf263cdc50a1686f39ded2ed
evgenii <evgenii@MacBook-Air-evgenii.local>
2021-04-16 14:52:13 +0600

    edit prod table

--- database/migrations/2021_04_11_110843_create_products_table.php
+++ database/migrations/2021_04_11_110843_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->foreignId('brand_id')->constrained('brands')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
             $table->string('preview', 128)->nullable();
-            $table->string('images_dir', 128)->nullable();
+            $table->string('preview_dir', 128)->nullable();
         });
     }
 
